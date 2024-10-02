e01f874195de35fe1f1bcbbf03f90dc2e7abb85f
Aafrith <139564438+Aafrith@users.noreply.github.com>
2024-10-02 18:54:32 +0530

    wordreplacement

---
+++ wordreplacement.py
@@ -0,0 +1,7 @@
+def replace_word():
+    str = "Hi i am Aafrith"
+    word_to_replace = input("Enter the word to replace: ")
+    new_word = input("Enter the new word: ")
+    print(str.replace(word_to_replace, new_word))
+
+replace_word()
